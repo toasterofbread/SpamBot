f3920412f59ea6a39b8c7e894b7ec8e024eeafc5
Talo Halton <Spectreseven1138@SCHOLAR1.oyis.ymca.org>
2019-10-10 21:54:35 +0900

    a

--- main_script.py
+++ main_script.py
@@ -10,21 +10,35 @@ async def on_ready():
     await client.change_presence(activity=discord.Game("Use '.spam ?' for info"))
     spam_running = False
 
+locked = False
+
 @client.command(pass_context=True)
 async def spam(ctx, mode = "11381138", *, rep = "a"):
-    print("ok")
-    if str.lower(mode) == "on":
+    global locked
+    if str.lower(mode) == "lock" and str(ctx.message.author.id) == "402344993391640578":
+        await ctx.send(ctx.message.author.mention + "  |  SpamBot has been locked")
+        locked = True
+    elif str.lower(mode) == "lock" and str(ctx.message.author.id) != "402344993391640578":
+        await ctx.send(ctx.message.author.mention + "  |  You do not have permission to use this command")
+    elif str.lower(mode) == "unlock" and str(ctx.message.author.id) == "402344993391640578":
+        await ctx.send(ctx.message.author.mention + "  |  SpamBot has been unlodked")
+        locked = False
+    elif str.lower(mode) == "unlock" and str(ctx.message.author.id) != "402344993391640578":
+        await ctx.send(ctx.message.author.mention + "  |  You do not have permission to use this command")
+    elif str.lower(mode) == "on" and not locked:
         await ctx.send(ctx.message.author.mention + "  |  SpamBot has been enabled")
         global spam_running
         spam_running = True
         while spam_running:
             await ctx.send(rep)
             time.sleep(0.5)
-    elif str.lower(mode) == "off":
+    elif str.lower(mode) == "off" and not locked:
         spam_running = False
         await ctx.send(ctx.message.author.mention + "  |  SpamBot has been disabled")
     elif str.lower(mode) == "11381138" or str.lower(mode) == "?" or str.lower(mode) == "help":
         await ctx.send(ctx.message.author.mention + "  |  This bot will send a message of your choice every half second when enabled\n \n**.spam on *message***  -  Enables spam bot with inputted message\n \n**.spam off**  -  Disables spam bot\n \n**.spam help**  -  Bot info")
+    elif locked:
+        await ctx.send(ctx.message.author.mention + "  |  SpamBot is currently locked")
     else:
         await ctx.send(ctx.message.author.mention + "  |  That is not the correct use of this command\n \nUse **.spam** for help")
 
